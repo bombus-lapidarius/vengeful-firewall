70a1099780ef1955d677d9ba5ac5483dd333154f
Tomislav Petricevic <tomislav@petricevic.me.uk>
2021-12-29 13:03:32 +0100

    Change http GET requests to http POST for api calls
    We are implementing a remote procedure call API, not a RESTful API.

--- src/main/WebApi/ConfigController.cs
+++ src/main/WebApi/ConfigController.cs
@@ -9,28 +9,28 @@ namespace ForcefulFi.WebApi
     {
         // TODO: top level handler (api/v0/config)
 
-        [HttpGet] // TODO: HTTP POST?
+        [HttpPost] // use POST, as this is a remote procedure call
         public ActionResult<IEnumerable<string>> Apply() // TODO: complete path
         {
             // this is just for demonstration purposes
             return new string[] { "You", "put", "me", "here" }; // TODO: async?
         }
 
-        [HttpGet] // TODO: HTTP POST?
+        [HttpPost] // use POST, as this is a remote procedure call
         public ActionResult<IEnumerable<string>> Edit()
         {
             // this is just for demonstration purposes
             return new string[] { "I", "am", "a", "config", "file" }; // TODO: async?
         }
 
-        [HttpGet] // TODO: HTTP POST?
+        [HttpPost] // use POST, as this is a remote procedure call
         public ActionResult<IEnumerable<string>> Replace()
         {
             // this is just for demonstration purposes
             return new string[] { "You", "put", "me", "here" }; // TODO: async?
         }
 
-        [HttpGet] // TODO: HTTP POST?
+        [HttpPost] // use POST, as this is a remote procedure call
         public ActionResult<IEnumerable<string>> Show()
         {
             // this is just for demonstration purposes
